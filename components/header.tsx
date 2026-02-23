b2a17c7428c9f6b21bcd4f1e059281605751028c
BRENT¥ <brentlemmuel2@gmail.com>
2026-02-23 16:52:40 +0800

    header UI update

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import { SidebarTrigger } from "./ui/sidebar";
+
+export const Header = () => {
+  return (
+    <div className="bg-blue-200 h-12 flex items-center px-4">
+      <SidebarTrigger />
+    </div>
+  );
+};
